a90bd162442bce5daf09343c7d3ca0b73f6668f5
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-05-13 19:31:10 +0300

    Setup IndexMassVC screen and add IndexVC file

---
+++ MassIndexApp/ViewControllers/IndexMassViewController.swift
@@ -0,0 +1,34 @@
+//
+//  IndexMassViewController.swift
+//  MassIndexApp
+//
+//  Created by Артур Сахбиев on 13.05.2022.
+//
+
+import UIKit
+
+class IndexMassViewController: UIViewController {
+
+    @IBOutlet var weightTF: UITextField!
+    @IBOutlet var hightTF: UITextField!
+    
+    @IBOutlet var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
