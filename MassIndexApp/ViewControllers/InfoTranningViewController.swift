5a50136f6e6018059997072e1bfb0edd05c0c187
Artur Sakhbiev <56787456+Takoivot@users.noreply.github.com>
2022-05-15 11:17:46 +0300

    Setups VC and cleaning code

--- MassIndexApp/ViewControllers/InfoTranningViewController.swift
+++ MassIndexApp/ViewControllers/InfoTranningViewController.swift
@@ -11,21 +11,14 @@ class InfoTranningViewController: UIViewController {
     
     var nameTranning: String!
 
+    @IBOutlet var photoWhatToDo: UIImageView!
+    
+    @IBOutlet var infoTraning: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
