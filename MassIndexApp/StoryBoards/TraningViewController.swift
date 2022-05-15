c863a480e7b3bdabed3d04b6ead3cb0d42f7cde4
MakGris <maxgrishenko@gmail.com>
2022-05-15 19:45:56 +0700

    add transaction array of trainings from resultViewController to trainingViewController

--- MassIndexApp/StoryBoards/TraningViewController.swift
+++ MassIndexApp/StoryBoards/TraningViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class TraningViewController: UITableViewController {
-
-    let namesTranning = ["Tranning1","Tranning2","Tranning3","Tranning4","Tranning5",]
-    let numbersOfApproaches = ["3x15","3x14","3x13","3x12","3x11",]
+    var trainings:[Training]!
+//    let namesTranning = ["Tranning1","Tranning2","Tranning3","Tranning4","Tranning5",]
+//    let numbersOfApproaches = ["3x15","3x14","3x13","3x12","3x11",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,17 +19,17 @@ class TraningViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        namesTranning.count
+        trainings.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "infoTraningID", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        let nameTranning = namesTranning[indexPath.row]
-        let numberOfApproaches = numbersOfApproaches[indexPath.row]
+        let nameTraining = trainings[indexPath.row].name
+        let numberOfApproaches = "Кол-во подходов: \(trainings[indexPath.row].sets). Кол-во повторений: \(trainings[indexPath.row].reps)"
         
-        content.text = nameTranning
+        content.text = nameTraining
         content.secondaryText = numberOfApproaches
         
         cell.contentConfiguration = content
@@ -44,7 +45,7 @@ class TraningViewController: UITableViewController {
      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
          guard let infoTranningVC = segue.destination as? InfoTranningViewController else {return}
          guard let indexPath = tableView.indexPathForSelectedRow else {return}
-         let nameTranning = namesTranning[indexPath.row]
+         let nameTranning = trainings[indexPath.row].name
          infoTranningVC.nameTranning = nameTranning
       
      }
